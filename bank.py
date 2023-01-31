20f2db0ac282675c1abd32bbfe9626763cf7b1f0
DONOHUE <Eilis.Donohue@woodgroup.com>
2023-01-31 15:32:34 +0000

    Initial attempt wk2

---
+++ bank.py
@@ -0,0 +1,18 @@
+# bank.py
+import math
+
+# ask user for input
+input1 = input("Enter amount1(in cent): ")
+input2 = input("Enter amount2(in cent): ")
+
+input1 = int(input1)
+input2 = int(input2)
+
+# add the inputs
+total_cent = input1 + input2
+
+# convert to euro
+total_euro = total_cent / 100
+
+# print total
+print("The sum of these is €",total_euro)
