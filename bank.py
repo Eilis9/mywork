e49901c9ad42425451645ebb2837653e677549ef
Eilis9 <G00006088@atu.ie>
2023-01-31 17:14:18 +0000

    week2 work

--- bank.py
+++ bank.py
@@ -2,11 +2,8 @@
 import math
 
 # ask user for input
-input1 = input("Enter amount1(in cent): ")
-input2 = input("Enter amount2(in cent): ")
-
-input1 = int(input1)
-input2 = int(input2)
+input1 = int(input("Enter amount1(in cent): "))
+input2 = int(input("Enter amount2(in cent): "))
 
 # add the inputs
 total_cent = input1 + input2
