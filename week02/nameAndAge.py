12d040b6a61fec3c73a7fefc8ffa3d92421596a5
Eilis9 <G00006088@atu.ie>
2023-02-05 12:58:16 +0000

    week02

---
+++ week02/nameAndAge.py
@@ -0,0 +1,10 @@
+# nameAndAge.py
+# Author: Eilis Donohue
+# asks for input and prints formatted string
+
+name = input("What is your name:")
+age = input("What is your age:")
+
+print(f'Hello {name}, your age is {age}.')
+
+print(f'Hello {name},\t your age is {age}.')
