84d2971b012f0f678e894463b95677b3fbfbb2b2
Eilis9 <G00006088@atu.ie>
2023-03-07 13:57:00 +0000

    solution to week2 task

---
+++ week02/bank_solution.py
@@ -0,0 +1,24 @@
+''' bank.py
+Week 2 PANDS task assignment 
+Author: Eilis Donohue
+Prompts the user for 2 integer inputs 
+Adds these and outputs the answer in euro and cent
+'''
+
+
+# Prompt user for input
+input1 = int(input("Enter amount1(in cent): "))
+input2 = int(input("Enter amount2(in cent): "))
+
+# Add the inputs
+total_cent = input1 + input2
+
+# Convert total to a string type so that slicing can be applied
+total_cent_str = str(total_cent)
+# Last 2 digits of the string is the cent amount and everything else is euro.
+# This avoids float types and need for leading zeroes when using // and % 
+euro_amount = total_cent_str[:-2]
+cent_amount = total_cent_str[-2:]
+
+# Output the amount 
+print(f"The sum of these is €{euro_amount}.{cent_amount}")
